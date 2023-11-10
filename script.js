547a05e19d33f6875a866a49b40bf71779b92fe3
Michael <michael@dancearound.world>
2023-11-10 11:40:08 +0100

    Erster Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('call-btn').addEventListener('click', function() {
+    // Logik zum Starten/Beenden des Anrufs
+    alert('Anruf gestartet/beendet');
+});
+
+document.getElementById('send-btn').addEventListener('click', function() {
+    // Logik zum Senden von Nachrichten
+    alert('Nachricht gesendet');
+});
